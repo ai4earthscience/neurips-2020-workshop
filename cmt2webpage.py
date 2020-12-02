9818975d659220e4094a255a9ccab9212ebd1af7
johannah <jh1736@gmail.com>
2020-12-01 19:47:44 -0500

    added links - may have broken some things

--- cmt2webpage.py
+++ cmt2webpage.py
@@ -137,7 +137,7 @@ for xx, session in enumerate(sessions):
                             if x.startswith(st_with):
                                 target_link = 'papers/ai4earth_neurips_2020_%02d.pdf' %paper_id
                                 shutil.copy2(x, target_link)
-                                title = '<a href="{}">{}</a>'.format(os.path.join(sitename,target_link), title)
+                                title = '<p><a href="{}">{}</a></p>'.format(os.path.join(sitename,target_link), title)
                                 print(title)
                         # get the abstract
                         longform = abstracts[abstracts['Paper ID'] == paper_id]['Abstract'].to_numpy()[0]
@@ -146,7 +146,7 @@ for xx, session in enumerate(sessions):
 
             link = talk['Link'].to_numpy()[0]
             if type(link) == str:
-                author = '<a href="{}">{}</a>'.format(link.strip(), author.strip())
+                author = '<p><a href="{}">{}</a></p>'.format(link.strip(), author.strip())
 
             # split long abstracts/bios in to visible and "more" after 2 sentences
             # hacky 
