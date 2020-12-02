032990b987255d7d3178047f34ac6507ac33760b
johannah <jh1736@gmail.com>
2020-12-01 19:53:04 -0500

    added links - may have broken some things

--- cmt2webpage.py
+++ cmt2webpage.py
@@ -156,7 +156,9 @@ for xx, session in enumerate(sessions):
                 st = '. '.join(spl_str[:brk]) + '.'
                 en = '. '.join(spl_str[brk:]) 
                 # something about Deepfish messes up the html summary function 
-                if 'Deepfish' or 'Boots on the Ground' in title: 
+                if 'Deepfish' in title:
+                    longline = '.' .join(spl_str)
+                elif 'Boots on the Ground' in title: 
                     longline = '.' .join(spl_str)
                 else:
                     longline = """<p style="display:inline";>{}<details style="display:inline;"closed><summary>More</summary>{}</details></p>""".format(st, en)
