857b4bd972a26133dbeb4a47db8254daf593fccd
johannah <jh1736@gmail.com>
2020-12-01 19:08:48 -0500

    added weblink

--- cmt2webpage.py
+++ cmt2webpage.py
@@ -142,6 +142,9 @@ for xx, session in enumerate(sessions):
                         longform = abstracts[abstracts['Paper ID'] == paper_id]['Abstract'].to_numpy()[0]
 
             author = talk['Authors'].to_numpy()[0].title().replace('()', '')
+            link = talk['Link'].to_numpy()[0]
+            if type(link) == str:
+                author = '<a href="{}">{}</>'.format(link, author)
 
             # split long abstracts/bios in to visible and "more" after 2 sentences
             # hacky 
